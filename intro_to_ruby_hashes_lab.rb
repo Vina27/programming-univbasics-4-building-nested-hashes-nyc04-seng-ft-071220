eaca728a22f1d35c8801cff72a53702214d02938
Maysoon Tabali <mtab03@me.com>
2020-07-20 18:22:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,13 +15,6 @@ def monopoly_with_second_tier
     railroads => :pieces 4, :rent_in_dollars: {}, :names {}
   }
 end 
-{
-    railroads: {
-      pieces: 4,
-      rent_in_dollars: {},
-      names: {}
-    }
-  }
 
 
   
