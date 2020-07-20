a1c6883942eddca6fa2711ab5a1fa1a98d583348
Maysoon Tabali <mtab03@me.com>
2020-07-20 18:14:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
   base_hash = { :railroads => []}
   :railroads => {
     [:pieces "4"]
-    [:rent_in_dollars "{}"]
-    [:names "{}"]
+    [:rent_in_dollars '{}']
+    [:names '{}']
   }
 end 
   
